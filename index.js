96cb99bf62de338e7ad949db8e09d59f263108ca
gupta56x <113337289+gupta56x@users.noreply.github.com>
2023-01-09 22:19:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,5 +8,5 @@ app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
 
 app.listen(process.env.PORT || 5000, () => {
-  console.log("Started express server at port 5000");
+  console.log("Start express server at port 5000");
 });
